7dbd04a3096fc9504728249bd512edcb0bc8b461
Temurlan <erkinbaevtemurlan@gmail.com>
2024-08-14 20:00:31 +0600

    New

--- Main/models.py
+++ Main/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Categorys(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, unique=True, verbose_name="Название")
 
     def __str__(self):
         return self.title
 
 
 class Hashtag(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, unique=True, verbose_name="Название")
 
     def __str__(self):
         return self.title
@@ -17,26 +17,26 @@ class Hashtag(models.Model):
 
 class Publication(models.Model):
     Hashtag = models.ManyToManyField(Hashtag, null=True)
-    name = models.CharField(max_length=250)
-    short_descriptions = models.TextField()
-    images = models.ImageField()
+    name = models.CharField(max_length=250, verbose_name="Название")
+    short_descriptions = models.TextField(verbose_name="Краткое описание")
+    images = models.ImageField(verbose_name="Изображение")
     my_date = models.DateField(null=True)
     is_activ = models.BooleanField(null=True)
 
 
 class Publication_Detail(models.Model):
-    Categorys = models.ForeignKey(Categorys, on_delete=models.CASCADE, null=True, related_name='publication')
-    Hashtag = models.ManyToManyField(Hashtag, null=True)
-    name = models.CharField(max_length=250)
-    descriptions = models.TextField()
-    images = models.ImageField()
+    Categorys = models.ForeignKey(Categorys, on_delete=models.CASCADE, null=True, related_name='publication', verbose_name="Категория")
+    Hashtag = models.ManyToManyField(Hashtag, null=True, verbose_name="Хэштег")
+    name = models.CharField(max_length=250, verbose_name="Название")
+    descriptions = models.TextField(verbose_name="Описание")
+    images = models.ImageField(verbose_name="Изаброжение")
     date = models.DateField(null=True)
 
 
 class PublicationComment(models.Model):
     Publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    text = models.TextField()
+    name = models.CharField(max_length=200, verbose_name="Название")
+    text = models.TextField(verbose_name="Тест")
     created_at = models.DateField(auto_now_add=True)
 
 
--- Main/views.py
+++ Main/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from django.views.generic import View
 from django.db.models import Q
 from django.shortcuts import redirect
-from Main.models import Publication, Publication_Detail, PublicationComment
+from Main.models import Publication, Publication_Detail, PublicationComment, Hashtag
 
 
 class HomeView(TemplateView):
@@ -12,6 +12,7 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
             context = {
                 'Publication_list': Publication.objects.filter(is_activ=True),
+
 
                      }
             return context
@@ -24,7 +25,7 @@ class HomeSearchView(TemplateView):
         search_word = self.request.GET['search']
         context = {
             'Publication_list': Publication.objects.filter(is_activ=True).filter(
-            Q(name__icontains=search_word) | Q(short_descriptions__icontains=search_word),
+            Q(name__icontains=search_word) | Q(short_descriptions__icontains=search_word) | Q(Hashtag=search_word),
             )
         }
         return context
@@ -42,6 +43,7 @@ class PublicationDetailView(TemplateView):
             context = {
                 'Publication_Detail': Publication_Detail.objects.get(
                     id=publication_pk),
+                'Publication_list': Publication.objects.all(),
 
                     }
             return context
